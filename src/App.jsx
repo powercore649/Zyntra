fc338e26c45a29577629db8510aac29d09c6da8d
powercore649 <126120353+powercore649@users.noreply.github.com>
2026-05-09 19:50:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-import Changelog from './pages/Changelog'; // <-- AJOUT
+import Changelog from './pages/Changelog'; // <-- ajouté
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />} />
-        <Route path="/changelog" element={<Changelog />} /> {/* <-- AJOUT */}
+        <Route path="/changelog" element={<Changelog />} /> {/* <-- ajouté */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
